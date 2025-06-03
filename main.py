b3d14cf9f558ea305f57f3ebb817dc9004d6bbc7
N-888 <mamambell@gmail.com>
2025-06-03 03:59:36 +0300

    Zerocoder1_test
    небольшая проверка полученных знаний OG02

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print ("Hi! world!")
+while True:
+    try:
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
+        print("Результат умножения этих чисел:", num1 * num2)
+    except ValueError:
+        user_input = input("Ошибка: Введите корректные числа.")

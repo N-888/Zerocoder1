e83659d71ae65d62bf10b69516e4f549da8bed59
N-888 <mamambell@gmail.com>
2025-06-09 04:36:53 +0300

    function area num1, num2

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+def area(num1, num2):
+    return num1 * num2
+
+
 while True:
     try:
         num1 = float(input("Введите первое число: "))
         num2 = float(input("Введите второе число: "))
-        print("Результат умножения этих чисел:", num1 + num2)
+
+        print("Результат сложения этих чисел:", num1 + num2)
+        print("Площадь (произведение этих чисел):", area(num1, num2))
     except ValueError:
-        user_input = input("Ошибка: Введите корректные числа.")
+        print("Ошибка: введите корректные числа.")

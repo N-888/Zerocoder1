6c62c2d0a41939421c3bcf5c051a436816d88383
N-888 <mamambell@gmail.com>
2025-06-09 03:53:58 +0300

    Summary_OG03

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ while True:
     try:
         num1 = float(input("Введите первое число: "))
         num2 = float(input("Введите второе число: "))
-        print("Результат умножения этих чисел:", num1 * num2)
+        print("Результат умножения этих чисел:", num1 + num2)
     except ValueError:
         user_input = input("Ошибка: Введите корректные числа.")
